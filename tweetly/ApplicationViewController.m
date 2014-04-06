e2f364390565e0dd7eb199ef2311be1adb6f89c9
Anna Do <annado@gmail.com>
2014-04-06 16:07:29 -0700

    add drop shadow to content view, private var naming convention

--- tweetly/ApplicationViewController.m
+++ tweetly/ApplicationViewController.m
@@ -30,20 +30,17 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // init Menu view
-        self.menuViewController = [[MenuViewController alloc] init];
-        [self addChildViewController:self.menuViewController];
+        _menuViewController = [[MenuViewController alloc] init];
+        [self addChildViewController:_menuViewController];
         
         
-        // init Timeline view
-        TimelineViewController *timelineViewController = [[TimelineViewController alloc] init];
-        UINavigationController *navigationController = [[UINavigationController alloc]
-                                                        initWithRootViewController:timelineViewController];
-
-        self.timelineViewController = timelineViewController;
-        self.timelineNavController = navigationController;
-        self.profileViewController = [[ProfileViewController alloc] initWithUser:[User currentUser]];
-        [self addChildViewController:self.timelineNavController];
-        [self addChildViewController:self.profileViewController];
+        // init Timeline/Profile view
+        _timelineViewController = [[TimelineViewController alloc] init];
+        _timelineNavController = [[UINavigationController alloc]
+                                                        initWithRootViewController:_timelineViewController];
+        _profileViewController = [[ProfileViewController alloc] initWithUser:[User currentUser]];
+        [self addChildViewController:_timelineNavController];
+        [self addChildViewController:_profileViewController];
     }
     return self;
 }
@@ -54,18 +51,18 @@
     
     [self closeMenu];
     if ([url.host isEqualToString:@"timeline"]) {
-        self.timelineViewController.mentions = NO;
-        [self.contentView bringSubviewToFront:self.timelineNavController.view];
+        _timelineViewController.mentions = NO;
+        [_contentView bringSubviewToFront:_timelineNavController.view];
     } else if ([url.host isEqualToString:@"mentions"]) {
-        self.timelineViewController.mentions = YES;
-        [self.contentView bringSubviewToFront:self.timelineNavController.view];
+        _timelineViewController.mentions = YES;
+        [_contentView bringSubviewToFront:_timelineNavController.view];
     } else if ([url.host isEqualToString:@"profile"]) {
         NSLog(@"profiles link");
         if (parameters[@"user"]) {
             NSLog(@"profile for user: %@", parameters[@"user"]);
         } else {
             NSLog(@"profile for currentUser");
-            [self.contentView bringSubviewToFront:self.profileViewController.view];
+            [_contentView bringSubviewToFront:_profileViewController.view];
         }
     }
 }
@@ -74,13 +71,19 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    UIView *mainView = self.timelineNavController.view;
-    UIView *menuView = self.menuViewController.view;
-    [self.contentView addSubview:self.profileViewController.view];
+    UIView *mainView = _timelineNavController.view;
+    UIView *menuView = _menuViewController.view;
+    [_contentView addSubview:_profileViewController.view];
     [self.view addSubview:menuView];
-    [self.contentView addSubview:mainView];
-    [self.view bringSubviewToFront:self.contentView];
-    [self.contentView bringSubviewToFront:self.timelineNavController.view];
+    [_contentView addSubview:mainView];
+    [self.view bringSubviewToFront:_contentView];
+    [_contentView bringSubviewToFront:_timelineNavController.view];
+    
+    // set drop shadow
+    [_contentView.layer setCornerRadius:3];
+    [_contentView.layer setShadowColor:[UIColor grayColor].CGColor];
+    [_contentView.layer setShadowOpacity:0.8];
+    [_contentView.layer setShadowOffset:CGSizeMake(-2, -2)];
 
     // Gesture recognizer
     UIPanGestureRecognizer *panGestureRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(onPan:)];
@@ -98,21 +101,21 @@
     CGPoint point = [panGestureRecognizer locationInView:self.view];
     CGPoint velocity = [panGestureRecognizer velocityInView:self.view];
     
-    CGRect frame = self.contentView.frame;
+    CGRect frame = _contentView.frame;
 
     if (panGestureRecognizer.state == UIGestureRecognizerStateBegan) {
-//        NSLog(@"Gesture began at: %@", NSStringFromCGPoint(point));
-        self.panning = (point.x > 0 && point.x - frame.origin.x < frame.size.width/4);
+        _panning = (point.x > 0 && point.x - frame.origin.x < frame.size.width/4);
         self.startingX = point.x;
     } else if (panGestureRecognizer.state == UIGestureRecognizerStateChanged) {
-        if (point.x > 0 && self.panning) {
+        if (_panning && point.x > 0) {
             frame.origin.x = point.x - self.startingX;
-            self.contentView.frame = frame;
+            if (frame.origin.x > 0) {
+                _contentView.frame = frame;
+            }
         }
     } else if (panGestureRecognizer.state == UIGestureRecognizerStateEnded) {
-//        NSLog(@"Gesture ended: %@", NSStringFromCGPoint(point));
-        if (self.panning) {
-            self.panning = NO;
+        if (_panning) {
+            _panning = NO;
 
             NSInteger minWidthMult = 1;
             if (velocity.x < 0) { // panning to the left
@@ -131,7 +134,7 @@
 
 - (void)closeMenu
 {
-    CGRect frame = self.contentView.frame;
+    CGRect frame = _contentView.frame;
     if (frame.origin.x == 0) {
         return;
     }
@@ -139,25 +142,21 @@
     frame.origin.x = 0;
     [UIView animateWithDuration:0.5
                      animations:^{
-                         self.contentView.frame = frame;
+                         _contentView.frame = frame;
                      }
-                     completion:^(BOOL finished){
-                         if(finished) {
-                             self.panning = NO;
-                         }
-                     }];
+                     completion:nil];
 
 }
 
 - (void)openMenu
 {
-    CGRect frame = self.contentView.frame;
+    CGRect frame = _contentView.frame;
     frame.origin.x = frame.size.width - frame.size.width/6;
-    self.contentView.frame = frame;
+    _contentView.frame = frame;
 
     [UIView animateWithDuration:0.5
                      animations:^{
-                         self.contentView.frame = frame;
+                         _contentView.frame = frame;
                      }
                      completion:nil];
 
